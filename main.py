c395ec828243df40f9d959facfc960ef80a92196
umoho <my.umoho@foxmail.com>
2022-03-04 22:29:13 +0800

    Changes

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 import logging
+import sys
+import getopt
 
 import const
 import server
 import config
 from log import log_init
 
-# Log
-log_init()
 
-logging.info(const.MOTD)
-logging.info(f'Welcome to use {const.NAME} (version {const.VERSION}, created by {const.AUTHOR})')
+def main(argv):
+    # Log
+    log_init()
 
-# Confi
+    # Welcome
+    logging.info(const.MOTD)
+    logging.info(f'Welcome to use {const.NAME} (version {const.VERSION}, created by {const.AUTHOR})')
 
-conf = config.Config.read(file_path='./config.yml')
+    # Option
+    opts = None
+    try:
+        opts, args = getopt.getopt(argv, 'hc:', ['help', 'config='])
+    except getopt.GetoptError as e:
+        logging.exception(e)
+        exit(2)
 
-# Http Server
-http_server = server.HTTPServer(config=conf)
+    conf = None
 
-http_server.run()
+    for opt, arg in opts:
+        # Config
+        if opt in ('-c', '--config'):
+            conf = config.Config.read(file_path=arg)
+            logging.info('Use user config file')
 
-# TODO http_server.stop()
+    if not opts:
+        conf = config.Config.read(file_path='./config.yml')
+        logging.info('Use default config file')
+
+    # Http Server
+    http_server = server.HTTPServer(config=conf)
+
+    http_server.run()
+
+    # TODO http_server.stop()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
